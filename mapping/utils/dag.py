d49ff087fba4b8ffb519dc7bd67fa1ceb40cc750
Jacob Emmerson <84752408+jacobemmerson@users.noreply.github.com>
2026-06-15 14:03:56 -0400

    update: dag.py with new CSVs; shift focus from using results

--- mapping/utils/dag.py
+++ mapping/utils/dag.py
@@ -18,59 +18,65 @@ with open(os.path.join(os.path.dirname(__file__), '../..', 'models', 'models.jso
     model_res = json.load(f)
 
 def build(columns=None):
-    # merge benchmark --measures--> harm
+    # merge harms + bench_measures_harm
     out = (
-        bmh
-        .merge(harms[["parent_id", "harm_id", "label"]], on="harm_id")
-        .rename(columns={"label": "ev_label", "parent_id": "parent_harm_id"})
-        .assign(
-            parent_harm_id=lambda df: df["parent_harm_id"].astype(str).str[:-2],
-            harm_root_id=lambda df: df["harm_id"].astype(str).str[:-2] + "00"
-        )
+        harms
+        .rename(columns={
+            'parent_id' : 'parent_harm_id', 
+            'version' : 'harm_version', 
+            'notes' : 'harm_notes'
+            })
+        .merge(
+            bmh.drop(columns='ev_id').rename(columns={'version' : 'bmh_version', 'notes' : 'bmh_notes', 'annotator' : 'bmh_annotator', 'edge_id' : 'bmh_edge'}), 
+            on="harm_id")
+        .rename(columns=lambda x: x.strip().lower().replace(':', '').replace(' ', '_'))
     )
 
-    # merge parents + harm label + evidence label
+    # self-merge to get parent labels
     out = (
         out
         .merge(
-            harms[["harm_id", "label"]]
-            .rename(columns={"harm_id": "parent_harm_id", "label": "parent_harm_label"}),
-            on="parent_harm_id",
-            how="left",
-            suffixes=("_bmh", "_harm")
-        )
-        .merge(
-            harms[["harm_id", "label"]]
-            .rename(columns={"harm_id": "harm_root_id", "label": "harm_label"}),
-            on="harm_root_id",
-            how="left",
+            harms[['harm_id', 'label']].rename(
+                columns={'harm_id' : 'parent_harm_id', 'label' : 'parent_label'}
+            ),
+            on='parent_harm_id',
+            how='left'
         )
     )
 
-    # merge provisions + provision parents + citations
+    # merge with provision_addresses_harm
     out = (
         out
-        .merge(pah[["provision_id", "harm_id", "coverage", "justification"]], on="harm_id")
-        .merge(provisions, on="provision_id").rename(columns={"parent_id": "parent_prov_id"})
         .merge(
-            provisions[["provision_id", "citation"]].rename(columns={"provision_id" : "parent_prov_id", "citation" : "parent_citation"}),
-            on="parent_prov_id",
-            how="left"
+            pah.rename(columns={'notes' : 'pah_notes', 'annotator' : 'pah_annotator', 'edge_id' : 'pah_edge'}),
+            on='harm_id',
+            how='left'
+        )
+        .drop(columns='provision_version')
+    )
+
+    # merge with provisions + self-merge with parent citations
+    out = (
+        out
+        .merge(
+            provisions.rename(columns={'version': 'provision_version', 'notes' : 'provision_notes', 'parent_id' : 'provision_parent_id'}),
+            on='provision_id',
+            how='left'
+        )
+        .merge(
+            provisions[['provision_id', 'citation']].rename(columns={'provision_id' : 'provision_parent_id', 'citation' : 'parent_citation'}),
+            on='provision_parent_id',
+            how='left'
         )
     )
-    
     columns = columns if columns else out.columns
     return out[columns]
 
 if __name__ == "__main__":
+    selected = ['label', 'parent_label', 'citation', 'parent_citation', 'harm_domain', 'bench_title', 'binding_force', 'harm_id', 'coverage', 'strength', 'parent_harm_id', 'ev_id', 'provision_id', 'provision_parent_id', 'benchmark_id']
     out = build()
-
-    # link to results
-    #benchmarks = out['benchmark_id'].apply(lambda x: bench_map[x])
-
-    #results = {}
-    #for model in model_res:
-    #    results[model['name']] = ([(b, model['scores'][b]) for b in benchmarks if b in model['scores']])
-
-    print(out.T)
-    #print(results)
+    print(out.shape)
+    out = out[selected]
+    print(out.isna().sum(axis=0))
+    out = out.dropna(subset=['citation'])
+    print(out.head(2))
