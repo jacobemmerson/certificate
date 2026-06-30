4136ab7f5f308dd69c8da7093b42500f3a5e7001
jacobemmerson <emmerson.jacob@gmail.com>
2026-06-30 17:25:09 +0000

    fix: scripts

--- scripts/run_adversarial_slurm.sh
+++ scripts/run_adversarial_slurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=shb-adversarial
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:l40s:4
+#SBATCH --gres=gpu:a100:4
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=128G
 #SBATCH --time=24:00:00
