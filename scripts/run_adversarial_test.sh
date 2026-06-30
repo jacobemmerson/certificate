235ef63d344dd831a34fcf4522ebc4663ba6a4e5
jacobemmerson <emmerson.jacob@gmail.com>
2026-06-30 17:43:54 +0000

    update: gpu

--- scripts/run_adversarial_test.sh
+++ scripts/run_adversarial_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=shb-test
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:a5000:1
+#SBATCH --gres=gpu:nvidia_h100_80gb_hbm3_1g.10gb:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
 #SBATCH --time=00:30:00
